b2f9c69cdd4e99bbd76207185ccf0f7d43baad39
John Cardarelli <stressed@desserts.yodel.space>
2018-05-19 19:36:20 -0700

    Removed failing example test case

--- tests/equality.sh
+++ tests/equality.sh
@@ -4,13 +4,6 @@
 testEquality() {
   assertEquals 1 1
 }
-
-testPartyLikeItIs1999() {
-  year=`date '+%Y'`
-  assertEquals "It's not 1999 :-(" \
-      '1999' "${year}"
-}
-
  
 # load shunit2
 . shunit2/shunit2
