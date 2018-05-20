5c9d35ec33c7c5aed8dedc1e92c51966337b535b
John Cardarelli <stressed@desserts.yodel.space>
2018-05-19 19:35:06 -0700

    Added simple test case

---
+++ tests/equality.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+# file: examples/party_test.sh
+ 
+testEquality() {
+  assertEquals 1 1
+}
+
+testPartyLikeItIs1999() {
+  year=`date '+%Y'`
+  assertEquals "It's not 1999 :-(" \
+      '1999' "${year}"
+}
+
+ 
+# load shunit2
+. shunit2/shunit2
