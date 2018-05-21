54c5432d64c8a7d0dddb705be507eee5fd6b56cb
John Cardarelli <stressed@desserts.yodel.space>
2018-05-20 17:27:21 -0700

    Modified script to use virtualenv instead of virtualenvwrapper

--- venv-ansible.sh
+++ venv-ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ANSIBLE_DIR=/opt/ansible
-APT_PREREQS=(python python-pip virtualenvwrapper)
+APT_PREREQS=(python python-pip virtualenv)
 VENV_NAME=ansible
 VENV_DIR=$HOME/.virtualenvs/$VENV_NAME
 
@@ -17,27 +17,26 @@ sudo apt-get update
 echo "Installing ${APT_PREREQS[@]} packages with apt-get"
 sudo apt-get install ${APT_PREREQS[@]} -y
 
-
 if [[ -d $VENV_DIR ]]; then
   echo "ansible virtualenv already exists at $VENV_DIR"
 else
-  echo "Creating virtualenv $VENV_NAME (${VENV_DIR}) with virtualenvwrapper"
-
   # Set the PROJECT_HOME shell variable to the name of the directory where projects should be created.
   PROJECT_HOME=$HOME/.virtualenvs
 
-  echo "Setting up virtualenvwrapper"
-  source $(whereis virtualenvwrapper | cut -f 2 -d':')/virtualenvwrapper.sh
-
-  echo "mkproject $VENV_NAME"
-  mkproject $VENV_NAME
+  echo "Creating virtualenv $VENV_NAME (${VENV_DIR}) with virtualenv"
+  virtualenv $VENV_NAME
 
   if [[ $? -ne 0 ]]; then
-    echo "The mkproject command exited with non-zero response code."
+    echo "The virtualenv command exited with non-zero response code."
     exit 1
   else
+    echo "source ${VENV_DIR}/${VENV_NAME}/bin/activate"
+    source ${VENV_DIR}/${VENV_NAME}/bin/activate
+
     echo "installing ansible"
-    pip install ansible -t $VENV_DIR --upgrade
+    pip install ansible
   fi
+
+
 fi
 
